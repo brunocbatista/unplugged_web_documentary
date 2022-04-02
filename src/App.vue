1810d7deacb573177489974d68164fa04fa59d86
brunocbatista <bruncbatista@gmail.com>
2022-04-01 22:54:06 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav-bar/>
+  <div class="bg-white p-14 flex items-center justify-center" style="background-image: url('https://i.gifer.com/AaQb.gif');background-size: cover;background-position: center;background-repeat: no-repeat;">
+    <video class="w-3/6" controls>
+      <source src="https://www.youtube.com/watch?v=TKqa-ZXGKYA&ab_channel=30PRAUM" type="video/mp4">
+    </video>
+  </div>
+  <div class="items-center justify-center text-center p-24">
+    <p class="text-4xl">"Aqui iria alguma frase de efeito"</p>
+    <p class="text-lg">Por fulano</p>
+  </div>
+</template>
+
+<script setup>
+import NavBar from '@/components/ui/NavBar.vue'
+</script>
+
+<style>
+@import '@/assets/base.css';
+</style>
---
+++ src/components/ui/NavBar.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav class="container flex justify-between px-4 py-8 mx-auto">
+    <div class="hidden space-x-8 md:flex">
+      <a href="">HOME</a>
+      <a href="">ESTATÍSTICAS</a>
+      <a href="">ALTERNATIVAS</a>
+    </div>
+    <div class="flex md:hidden">
+      <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="w-6 h-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+      >
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h16M4 18h16"
+        />
+      </svg>
+    </div>
+    <div>
+      <h3 class="text-lg font-medium text-blue-500">Desconectados</h3>
+    </div>
+  </nav>
+  <!--Nav-->
+<!--  <nav class="bg-black-soft p-2 mt-0 fixed w-full z-10 top-0">-->
+<!--    <div class="container mx-auto flex flex-wrap items-center">-->
+<!--      <div class="flex w-full pt-2 content-center justify-between md:w-1/2 md:justify-start">-->
+<!--        <ul class="list-reset flex justify-between flex-1 md:flex-none items-center">-->
+<!--          <li class="mr-3">-->
+<!--            <a class="inline-block no-underline hover:text-gray-200 py-2 px-4" href="#">HOME</a>-->
+<!--          </li>-->
+<!--          <li class="mr-3">-->
+<!--            <a class="inline-block no-underline hover:text-gray-200 py-2 px-4" href="#">ESTATÍSTICAS</a>-->
+<!--          </li>-->
+<!--          <li class="mr-3">-->
+<!--            <a class="inline-block no-underline hover:text-gray-200 py-2 px-4" href="#">ALTERNATIVAS</a>-->
+<!--          </li>-->
+<!--        </ul>-->
+<!--      </div>-->
+<!--      <div class="flex w-full md:w-1/2 justify-center md:justify-end text-white font-extrabold">-->
+<!--        <a class="text-white no-underline hover:text-white hover:no-underline" href="#">-->
+<!--          <span class="text-lg">Desconectados</span>-->
+<!--        </a>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  </nav>-->
+</template>
+<script setup>
+
+</script>
